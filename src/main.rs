d3dc4ef4258c0d1ed0857918b1c4f7f2e7c05a8e
Dylan Socolobsky <dylan.socolobsky@lambdaclass.com>
2024-11-20 13:04:43 -0300

    Move Opcodes to opcodes.rs and add some registers/conditions code to VM Struct

--- src/main.rs
+++ src/main.rs
@@ -1,239 +1,58 @@
 #![allow(clippy::unusual_byte_groupings)]
 #![allow(clippy::upper_case_acronyms)]
+
+mod opcodes;
+
 use std::fs;
+use crate::opcodes::Opcode;
 
 const MEMORY_SIZE: usize = 2_usize.pow(16);
+const REG_IDX_PC: usize = 8;
+const REG_IDX_COND: usize = 9;
 
-#[derive(Debug)]
-enum Argument {
-    Reg(u16),
-    Immediate(i16),
+enum ConditionFlag {
+    Pos,
+    Neg,
+    Zero,
+    None
 }
 
-#[derive(Debug)]
-enum Opcode {
-    ADD {
-        // Add (dr <- sr1 + sr2/imm5)
-        dr: u16,
-        sr1: u16,
-        sr2: Argument,
-    },
-    AND {
-        /// Bitwise And (dr <- sr1 & sr2/imm5)
-        dr: u16,
-        sr1: u16,
-        sr2: Argument,
-    },
-    BR {
-        // Branch (If n/z/p => pc += offset)
-        n: bool,
-        z: bool,
-        p: bool,
-        offset: i16,
-    },
-    JMP {
-        // Jump (pc <- base_r)
-        base_r: u16,
-    },
-    RET, // Return from subroutine (JMP) (pc <- r7)
-    JSR {
-        // Jump To Subroutine (pc <- pc + offset)
-        offset: i16,
-    },
-    JSRR, // Jump to Subroutine (pc <- base_r)
-    LD {
-        // Load Direct from memory (dr <- mem[pc+offset])
-        dr: u16,
-        offset: i16,
-    },
-    LDI {
-        // Load Indirect from memory (dr <- mem[mem[pc+offset]])
-        dr: u16,
-        offset: i16,
-    },
-    LDR {
-        // Load Base+Offset (dr <- mem[base_r + offset])
-        dr: u16,
-        base_r: u16,
-        offset: i16,
-    },
-    LEA {
-        // Load Effective Address (dr <- pc + offset)
-        dr: u16,
-        offset: i16,
-    },
-    NOT {
-        // Bitwise Not (dr <- ~sr)
-        dr: u16,
-        sr: u16,
-    },
-    RTI, // Return From Interrupt
-    ST {
-        // Store (mem[pc+offset] <- sr)
-        sr: u16,
-        offset: i16,
-    },
-    STI {
-        // Store Indirect (mem[mem[pc+offset]] <- sr)
-        sr: u16,
-        offset: i16,
-    },
-    STR {
-        // Store Base+Offset (mem[sr+offset] <- sr)
-        sr: u16,
-        base_r: u16,
-        offset: i16,
-    },
-    TRAP {
-        // Execute Trap
-        trap_vec: u16,
-    },
-    RESERVED, // Unused. Throws an Illegal Opcode Exception.
+struct VM {
+    registers: [u16; 10],
 }
 
-fn decode_instruction(instruction: u16) -> Option<Opcode> {
-    let op = (instruction >> 12) as u8; // Highest 4 bits
-    match op {
-        0b0001 => {
-            // ADD
-            let dr = instruction & 0b0000_111_000_0_00_000 >> 9;
-            let sr1 = instruction & 0b0000_000_111_0_00_000 >> 6;
-            if instruction & (1 << 5) == 0 {
-                // Use sr2 register as 2nd argument
-                let sr2 = instruction & 0b0000_000_000_0_00_111;
-                Some(Opcode::ADD {
-                    dr,
-                    sr1,
-                    sr2: Argument::Reg(sr2),
-                })
-            } else {
-                // Use imm5 as 2nd argument
-                let imm5 = instruction & 0b0000_000_000_0_11_111;
-                Some(Opcode::ADD {
-                    dr,
-                    sr1,
-                    sr2: Argument::Immediate(imm5 as i16),
-                })
-            }
+impl VM {
+    fn pc(&self) -> u16 {
+        self.registers[REG_IDX_PC]
+    }
+
+    fn set_pc(&mut self, new_pc: u16) {
+        self.registers[REG_IDX_PC] = new_pc;
+    }
+
+    fn advance_pc(&mut self) {
+        self.registers[REG_IDX_PC] += 1;
+    }
+
+    fn cond_flag(&self) -> ConditionFlag {
+        let r = self.registers[REG_IDX_COND];
+        if r == 1 << 0 {
+            ConditionFlag::Pos
+        } else if r == 1 << 1 {
+            ConditionFlag::Zero
+        } else if r == 1 << 2 {
+            ConditionFlag::Neg
+        } else {
+            ConditionFlag::None
         }
-        0b0101 => {
-            // AND , this is the same decode case as ADD
-            let dr = instruction & 0b0000_111_000_0_00_000 >> 9;
-            let sr1 = instruction & 0b0000_000_111_0_00_000 >> 6;
-            if instruction & (1 << 5) == 0 {
-                // Use sr2 register as 2nd argument
-                let sr2 = instruction & 0b0000_000_000_0_00_111;
-                Some(Opcode::AND {
-                    dr,
-                    sr1,
-                    sr2: Argument::Reg(sr2),
-                })
-            } else {
-                // Use imm5 as 2nd argument
-                let imm5 = instruction & 0b0000_000_000_0_11_111;
-                Some(Opcode::AND {
-                    dr,
-                    sr1,
-                    sr2: Argument::Immediate(imm5 as i16),
-                })
-            }
-        }
-        0b0000 => {
-            // BR  Conditional Branch
-            Some(Opcode::BR {
-                n: instruction & (1 << 11) != 0,
-                z: instruction & (1 << 10) != 0,
-                p: instruction & (1 << 9) != 0,
-                offset: (instruction & 0b1_1111_1111) as i16,
-            })
-        }
-        0b1100 => {
-            // JMP/RET
-            let base_r = instruction & 0b0000_000_111_0_00_000 >> 6;
-            if base_r == 0b111 {
-                Some(Opcode::RET)
-            } else {
-                Some(Opcode::JMP { base_r })
-            }
-        }
-        0b0100 => {
-            // JSR/JSRR
-            if instruction & (1 << 11) != 0 {
-                Some(Opcode::JSR {
-                    offset: (instruction & 0b111_11111) as i16,
-                })
-            } else {
-                Some(Opcode::JSRR)
-            }
-        }
-        0b0010 => {
-            // LD
-            Some(Opcode::LD {
-                dr: (instruction & 0b111_0_00000000) >> 9,
-                offset: (instruction & 0b1111_1111) as i16,
-            })
-        }
-        0b1010 => {
-            // LDI , decode same as LD
-            Some(Opcode::LDI {
-                dr: (instruction & 0b111_0_00000000) >> 9,
-                offset: (instruction & 0b1111_1111) as i16,
-            })
-        }
-        0b0110 => {
-            // LDR
-            Some(Opcode::LDR {
-                dr: (instruction & 0b111_000_000000) >> 9,
-                base_r: (instruction & 0b000_111_000000) >> 6,
-                offset: (instruction & 0b000_000_111111) as i16,
-            })
-        }
-        0b1110 => {
-            // LEA
-            Some(Opcode::LEA {
-                dr: (instruction & 0b111_0000_0000) >> 8,
-                offset: (instruction & 0b000_1111_1111) as i16,
-            })
-        }
-        0b1001 => {
-            // NOT
-            Some(Opcode::NOT {
-                dr: (instruction & 0b111_000_0_00000) >> 9,
-                sr: (instruction & 0b111_000_0_00000) >> 9,
-            })
-        }
-        0b1000 => Some(Opcode::RTI),
-        0b0011 => {
-            // ST
-            Some(Opcode::ST {
-                sr: (instruction & 0b111_0000_0000) >> 8,
-                offset: (instruction & 0b000_1111_1111) as i16,
-            })
-        }
-        0b1011 => {
-            // STI
-            Some(Opcode::STI {
-                sr: (instruction & 0b111_0000_0000) >> 8,
-                offset: (instruction & 0b000_1111_1111) as i16,
-            })
-        }
-        0b0111 => {
-            // STR
-            Some(Opcode::STR {
-                sr: (instruction & 0b111_000_000000) >> 9,
-                base_r: (instruction & 0b000_111_000000) >> 6,
-                offset: (instruction & 0b111111) as i16,
-            })
-        }
-        0b1111 => {
-            Some(Opcode::TRAP {
-                trap_vec: instruction & 0b1111_1111, // This is 0-extended, not sign-extended
-            })
-        }
-        0b1101 => Some(Opcode::RESERVED),
-        _ => {
-            println!("Unknown instruction {op}");
-            None
+    }
+
+    fn set_cond_flag(&mut self, cond: ConditionFlag) {
+        self.registers[REG_IDX_COND] = match cond {
+            ConditionFlag::Pos => 1 << 0,
+            ConditionFlag::Zero => 1 << 1,
+            ConditionFlag::Neg => 1 << 2,
+            ConditionFlag::None => 0,
         }
     }
 }
@@ -268,7 +87,7 @@ fn main() {
     let mut pc: usize = 0x3000;
     while pc < 0x3000 + 20 {
         // Arbitrary limit to debug for now
-        let op = decode_instruction(memory[pc]);
+        let op = Opcode::try_from(memory[pc]).expect("Unknown opcode");
         dbg!(op);
         pc += 1;
     }

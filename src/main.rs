9771d549ac98bff2abfddd5246addb407660cb20
Dylan Socolobsky <dylan.socolobsky@lambdaclass.com>
2024-11-22 15:22:43 -0300

    Remove code and fix keyboard input

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 #![allow(clippy::unusual_byte_groupings)]
 #![allow(clippy::upper_case_acronyms)]
 
+use termios::*;
+
 mod opcodes;
-mod terminal;
 mod util;
 mod vm;
 
@@ -10,8 +11,18 @@ use crate::vm::VM;
 use std::fs;
 
 fn main() {
+    // Terminal stuff
+    let stdin = 0;
+    let termios = termios::Termios::from_fd(stdin).unwrap();
+    let mut new_termios = termios.clone();
+    new_termios.c_iflag &= IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL | IXON;
+    new_termios.c_lflag &= !(ICANON | ECHO); // no echo and canonical mode
+    tcsetattr(stdin, TCSANOW, &mut new_termios).unwrap();
+
     let data: Vec<u8> = fs::read("2048.obj").expect("Failed to load file");
     let mut vm = VM::new(&data);
     vm.run();
-    terminal::restore_terminal_settings();
+
+    // Restore terminal to default settings
+    tcsetattr(stdin, TCSANOW, &termios).unwrap();
 }
